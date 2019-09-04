e323d1d81338aee828a92892474585428b993fb7
tjcjc <taijcjc@gmail.com>
2019-09-04 23:32:56 +0800

    add codable test

---
+++ SwiftCocoaFoundationTests/Codable/CodableTest.swift
@@ -0,0 +1,15 @@
+//
+//  CodableTest.swift
+//  SwiftCocoaFoundationTests
+//
+//  Created by JasonTai on 2019/9/4.
+//  Copyright © 2019 JasonTai. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class CodableTest: QuickSpec {
+    override func spec() {
+    }
+}
